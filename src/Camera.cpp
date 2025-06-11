15d3493367f7ee7ed18aeaedb1772b3f4e27a4e6
TomG22 <giallanzatom@gmail.com>
2025-06-10 20:39:47 -0700

    Fixed build issue and warnings

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -9,7 +9,7 @@ Camera::Camera(int width, int height, glm::vec3 position) {
 void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane) {
 	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 projection = glm::mat4(1.0f);
-	
+
 	view = glm::lookAt(Position, Position + Orientation, Up);
 	projection = glm::perspective(glm::radians(FOVdeg), (float)width / height, nearPlane, farPlane);
 
@@ -40,10 +40,10 @@ void Camera::Inputs(GLFWwindow* window) {
 		Position += speed * -Up;
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-		speed = 0.01;
+		speed = 0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) {
-		speed = 0.001;
+		speed = 0.001f;
 	}
 
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
@@ -60,7 +60,7 @@ void Camera::Inputs(GLFWwindow* window) {
 
 		float rotX = sensitivity * (float)(mouseY - (height / 2)) / height;
 		float rotY = sensitivity * (float)(mouseX - (width / 2)) / width;
-		
+
 		glm::vec3 newOrientation = glm::rotate(Orientation, glm::radians(-rotX), glm::normalize(glm::cross(Orientation, Up)));
 
 		if (abs(glm::angle(newOrientation, Up) - glm::radians(90.0f)) <= glm::radians(85.0f))
@@ -69,7 +69,7 @@ void Camera::Inputs(GLFWwindow* window) {
 		}
 
 		Orientation = glm::rotate(Orientation, glm::radians(-rotY), Up);
-		
+
 		glfwSetCursorPos(window, (width / 2), (height / 2));
 	}
 	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) {
--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -18,7 +18,8 @@ void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& la
 	for (unsigned int i = 0; i < elements.size(); i++) {
 		const VertexBufferElement element = elements[i];
 		GLCall(glEnableVertexAttribArray(i));
-		GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, layout.GetStride(), (const void*)offset));
+        const void* glOffset = reinterpret_cast<const void*>(static_cast<std::uintptr_t>(offset));
+		GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, layout.GetStride(), glOffset));
 		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
 	}
 }
